5914f40dc73381f49890eda11fb11ede1ce5992f
PhilLaptop <philngtn@gmail.com>
2021-04-04 01:35:00 -0400

    Hash practice full

--- Dictionary/HashTable.py
+++ Dictionary/HashTable.py
@@ -17,14 +17,30 @@ class HashTable:
 
     def set(self,key,value):
         address = self._hash(key)
+        # If there is no [] data in big [[],[],...,[]]
+        # set it empty
+        # bool returns [] = false, [1,2] = true
         if (not bool(self.data[address])):
-            self.data[address] = [key,value]
-            print(self.data)
+            self.data[address] = []        
+        
+        self.data[address].append([key,value])
+        
+        print(self.data)
+        return self.data
+
+    def get(self,key):
+        address = self._hash(key)
+        currentBucket = self.data[address]
+        if (currentBucket):
+            for i in range(len(currentBucket)):
+                if(currentBucket[i][0] == key):
+                    return currentBucket[i][1]
+        return None
 
 
-
-
-hashObj = HashTable(50)
-
+hashObj = HashTable(2)
 hashObj.set('grapes',19)
+hashObj.set('grapsdes',1249)
+a = hashObj.get('grapes')
 
+print(a)
