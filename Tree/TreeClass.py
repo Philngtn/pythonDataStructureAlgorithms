647f41a5c7493dbfc5665ba7c94cf4c75ffe37db
PhilLaptop <philngtn@gmail.com>
2021-04-27 03:12:33 -0400

    Add removed

--- Tree/TreeClass.py
+++ Tree/TreeClass.py
@@ -32,35 +32,86 @@ class BinarySeachTree:
 
         while(True):
             if(value < temp.value):
+                # Check left node
                 if (temp.left == None):
                     temp.left = new_node
                     return print("Add left", value)
-                else:
-                    temp = temp.left
-
-            elif(value >= temp.value):
+                temp = temp.left
+            else:
+                # Check right node
                 if (temp.right == None):
                     temp.right = new_node
                     return print("Add right", value)
-                else:
-                    temp = temp.right
+                temp = temp.right
 
 
 
-    def lookup(self):
-        pass
+    def lookup(self, value):
+        if (not self.root):
+            return print("Empty")
+        
+        temp = self.root
+        while(temp):
+            if (value < temp.value):
+                temp = temp.left
+            elif (value > temp.value):
+                temp = temp.right
+            elif (value == temp.value):
+                return temp
+        
+        return print("Found nothing")
+            
+    def remove(self,value):
+        if (not self.root):
+            return print("Empty Tree")
+        
+        if not self.lookup(value):
+            return print("Node not found")
+        
+        parent_node = None
+        current_node = self.root
 
-    def remove(self):
-        pass
-    
+        while(current_node):
+            if(value < current_node.value):
+                parent_node = current_node
+                current_node = current_node.left
+            elif value > current_node.value:
+                parent_node = current_node
+                current_node = current_node.right
+            elif current_node.value == value:
+                # Option 1: No right child 
+                if(current_node.right == None):
+                    # Remove root node -> root node to the left
+                    if(parent_node == None):
+                        self.root = current_node.left
+                    else:
+                        # Left side, if parent > current value  
+                        # make current LEFT child as parent's RIGHT child (NO RIGHT CHILD CASE) 
+                        if (current_node.value < parent_node.value):
+                            parent_node.left = current_node.left
+                        # Left side, if parent < current value  
+                        # make current LEFT child as parent's RIGHT child (NO RIGHT CHILD CASE)
+                        elif (current_node.value > parent_node.value):
+                            parent_node.right = current_node.left
+                            
+                # Option 2: Right child does't have a left child 
+                elif (current_node.right.left == None):
+                    if (parent_node == None):
+
+
+                    
+
+
     def print_tree(self):
         if self.root != None:
             self.printt(self.root)
 
     def printt(self,curr_node):
         if curr_node != None:
+           # Recusion print all left nodes 
            self.printt(curr_node.left)
            print(str(curr_node.value))
+           # Recusion print all right nodes
            self.printt(curr_node.right)
 
 tree = BinarySeachTree()
@@ -70,5 +121,7 @@ tree.insert(20)
 tree.insert(650)
 tree.insert(34)
 tree.insert(40)
+
+print(tree.lookup(34))
 
 tree.print_tree()
