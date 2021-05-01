78310998003092673dfd5e34e4444d9eae63754b
PhilLaptop <philngtn@gmail.com>
2021-05-01 03:40:13 -0400

    Implement array max heap + push

--- Tree/BinaryHeapClass.py
+++ Tree/BinaryHeapClass.py
@@ -1,25 +1,45 @@
-from typing import Sized
+from typing import Literal
 
 
 class MaxHeap:
 
     def __init__(self):
         self.heap = []
-
     
-    def parent(self,child):
-        return (child-1)/2
+    def parent(self,i):
+        if i == 0:
+            return 0
+        return (i-1)//2
 
-    def insert(self,value):
-        heap = self.heap
-        heap.insert(value)
-        
-        if(len(heap) == 0):
-            pass    
-        else:
-            added_node = heap[-1]
-                
-            while(True):
-                
+    def size_heap(self):
+        return len(self.heap)
+
+    def isEmpty(self):
+        return len(self.heap) == 0
+
+    def swap_up(self, i):
+        if (self.heap[self.parent(i)] < self.heap[i]):
+            temp  = self.heap[self.parent(i)]
+            self.heap[self.parent(i)] = self.heap[i]
+            self.heap[i] = temp
+            self.swap_up(self.parent(i))
+        return True
+
+    def push(self, value):
+        self.heap.append(value)
+        index = self.size_heap() - 1
+        self.swap_up(index)
+
+
+max_heap = MaxHeap()
+
+max_heap.push(3)
+max_heap.push(5)
+max_heap.push(4)
+max_heap.push(24)
+
+print(max_heap.heap)
+
+
 
     
