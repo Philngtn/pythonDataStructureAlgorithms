7c2859398b6f313e18c88a8555d01eadeaac3b81
PhilLaptop <philngtn@gmail.com>
2021-05-06 13:57:24 -0400

    Quicksort with duplicated values

--- Algorithm/Sorting/QuickSort.py
+++ Algorithm/Sorting/QuickSort.py
@@ -1,2 +1,47 @@
 def quick_sort(array):
-    pass
+    left = 0
+    right = len(array) - 1
+    _qs(array, left, right)
+
+
+def _swap(arr, a, b):
+    temp = arr[a]
+    arr[a] = arr[b]
+    arr[b] = temp
+
+
+def _qs(array, left, right):
+    # Where to top the array for sorting
+    index = _partition(array, left, right)
+    # 
+    if (left < index - 1):
+        _qs(array, left, index -1 )
+    if (index < right):
+        _qs(array, index, right)
+
+def _partition(array, left, right):
+    i = left
+    j = right
+    pivot = array[(left + right) // 2]
+
+    # When start crosses end stop (it divides the 2 half where left is smaller and right is bigger than pivot)
+    while (i <= j):
+        while(array[i] < pivot):
+            i+=1
+        while(array[j] > pivot):
+            j-=1  
+        # when array[i] >= pivot >= array[j] -> swap
+        # the condition of check in range
+        if (i <= j):
+            _swap(array, i, j)
+            i+=1
+            j-=1
+    # Return the point to chop the array
+    return i
+
+
+numbers = [0,0,1,2,2,1,0,1,2,2,1,0,1,1]
+
+print(numbers)
+quick_sort(numbers)
+print(numbers)
--- Testbasic.py
+++ Testbasic.py
@@ -1,3 +1,13 @@
 a = [2,54,213,24,42,1,7]
 
-print(a[4:])
+
+
+def swap(arr, a, b):
+    temp = arr[a]
+    arr[a] = arr[b]
+    arr[b] = temp
+
+
+print(a)
+swap(a, 1,2)
+print(a)
