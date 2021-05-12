9f62a9dafc13ca6668a3b515ff6b8d0f6843f864
PhilLaptop <philngtn@gmail.com>
2021-05-12 17:07:37 -0400

    Fibonacci Dynamic Programming

--- Algorithm/DynamicProgramming/FibonacciDynamicProgamming.py
+++ Algorithm/DynamicProgramming/FibonacciDynamicProgamming.py
@@ -10,5 +10,27 @@ def fibonacci(n):
         return n
     return fibonacci(n-1) + fibonacci(n-2)
 
+
+def fibonacciDynamic():
+    cache = {}
+    
+    def fib(n):
+        if n in cache:
+            return cache[n]
+        else:
+            if n < 2:
+                return n
+            else:
+                cache[n] = fib(n-1) + fib(n-2)
+                return cache[n]
+
+    return fib
+
+
+
+
 print(fibonacci(7))
 print(count)
+
+refFibo = fibonacciDynamic()
+print(refFibo(8))
